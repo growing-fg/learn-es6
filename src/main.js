a86728716ed0940d63fc89246c2217d2bb69f8ca
郭飞 <guofei@guofeideMacBook-Pro.local>
2016-05-29 18:15:54 +0800

    class

--- src/main.js
+++ src/main.js
@@ -1,50 +1,51 @@
+"use strict"
+
+class User {
+    /**
+     * 构造方法，在new对象时会被调用
+     */
+    constructor (username, email, password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    register () {
+        console.log(this.username + ' is now registered');
+    }
+
+    /**
+     * 定义表态方式，即类可直接调用的方法
+     */
+    static countUsers () {
+        console.log('There are 100 users');
+    }
+}
+
+let bob = new User('bob', 'bob@email.com', '12345');
+
+bob.register();
+
+User.countUsers();
+
+
 /**
- * the Let and var
+ * 类继承
  */
 
-function theVar () {
-    var test = 'outer var';
+class Member extends User {
+    constructor (username, email, password, memberPackage) {
+        // 调用父类方法
+        super(username, email, password);
 
-    if (1) {
-        var test = 'inner var';
-        console.log(test);
+        this.package = memberPackage;
     }
 
-    console.log(test);
-}
-
-function theLet () {
-    let test = 'outer let';
-
-    if (1) {
-        let test = 'inner let';
-        console.log(test);
+    getPackage () {
+        console.log(this.username + ' is subscribed to the ' + this.package + ' package');
     }
-
-    console.log(test);
 }
 
-
-theVar();
-console.log('-------------------');
-theLet();
-
-for (let i = 0; i < 10; i++) {
-    console.log(i);
-}
-//console.log(i);  // error: i is not defined
-
-// const
-
-const user = {};
-
-user['name'] = 'John';
-user.age = 25;
-
-console.log(user.name);
-console.log(user.age);
-
-//user = {}; // error: Assignment to constant variable
-
-//const user.address = "x road y room"; // error
-//console.log(user.address);
+let mike = new Member('mike', 'mike@email.com', '123', 'standard');
+mike.register();
+mike.getPackage();
