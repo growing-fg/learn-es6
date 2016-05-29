021580b99a26274bea825777e996084f849e1efd
郭飞 <guofei@guofeideMacBook-Pro.local>
2016-05-29 18:38:38 +0800

    模板字符串

--- src/main.js
+++ src/main.js
@@ -1,51 +1,16 @@
 "use strict"
 
-class User {
-    /**
-     * 构造方法，在new对象时会被调用
-     */
-    constructor (username, email, password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-
-    register () {
-        console.log(this.username + ' is now registered');
-    }
-
-    /**
-     * 定义表态方式，即类可直接调用的方法
-     */
-    static countUsers () {
-        console.log('There are 100 users');
-    }
+function template (context) {
+    return `<h1>This is template</h1>
+<b>name</b>: ${context.name}<br>
+<b>age</b>: ${context.age}<br>
+<p>${context.message}</p>`
 }
 
-let bob = new User('bob', 'bob@email.com', '12345');
+let html = template({
+    name: "Fred",
+    age: 28,
+    message: "I am so happy to learn es2015!"
+});
 
-bob.register();
-
-User.countUsers();
-
-
-/**
- * 类继承
- */
-
-class Member extends User {
-    constructor (username, email, password, memberPackage) {
-        // 调用父类方法
-        super(username, email, password);
-
-        this.package = memberPackage;
-    }
-
-    getPackage () {
-        console.log(this.username + ' is subscribed to the ' + this.package + ' package');
-    }
-}
-
-let mike = new Member('mike', 'mike@email.com', '123', 'standard');
-mike.register();
-mike.getPackage();
+document.getElementById('template').innerHTML = html;
