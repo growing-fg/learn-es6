f394d16351361a9ee28ee855cb111232bbed64e2
郭飞 <guofei@guofeideMacBook-Pro.local>
2016-05-29 17:28:28 +0800

    let

--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
-class User {
-    sayHello () {
-        console.log('Hello World!');
+/**
+ * the Let and var
+ */
+
+function theVar () {
+    var test = 'outer var';
+
+    if (1) {
+        var test = 'inner var';
+        console.log(test);
     }
+
+    console.log(test);
 }
+
+function theLet () {
+    let test = 'outer let';
+
+    if (1) {
+        let test = 'inner let';
+        console.log(test);
+    }
+
+    console.log(test);
+}
+
+
+theVar();
+console.log('-------------------');
+theLet();
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+console.log(i);  // error: i is not defined
